400e414446950d9a8f626745c5b6fe02b97f73bb
KPLUU <kph.luu@gmail.com>
2019-10-24 17:44:43 +0200

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-6.times do
-    doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: ["généraliste", "dermatologue", "dentiste", "ORL"].sample, zip_code: Faker::Address.zip_code)
-end
 
-12.times do
-    patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-end 
-
-24.times do
-    appointment = Appointment.create!(date: Faker::Date.in_date_period, doctor: doctor, patient: patient)
-end
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+City.destroy_all
 
 5.times do
-    city = City.create!(city: Faker::Address.city)
+    city = City.create(city: "Paris")
+    doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: ["généraliste", "dermatologue", "dentiste", "ORL"].sample, zip_code: Faker::Address.zip_code, city: city)
+    patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: city)
+    appointment = Appointment.create!(date: Faker::Date.in_date_period, doctor: doctor, patient: patient, city: city)
 end
+
